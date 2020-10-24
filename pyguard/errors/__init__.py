68a3c46e4f4e73bcfe6a505748646daeff11c8bd
Greyson Murray <greyson.murray@gmail.com>
2020-10-23 23:13:56 -0400

    create ArgumentIncongruityWarning initializer

--- pyguard/errors/__init__.py
+++ pyguard/errors/__init__.py
@@ -4,3 +4,18 @@ class ArgumentIncongruityWarning(Warning):
 	the function's number of parameters. This could mean both
 	overconstraining and underconstraining the method's parameters.
 	"""
+	def __init__(self, funcname, typecount, argcount):
+		if typecount > argcount:
+			self.msg = (
+			f"Enforcing {typecount} {'type' if typecount == 1 else 'types'} "
+			f"while only {argcount} {'argument exists' if argcount == 1 else 'arguments exist'}. "
+			)
+		elif typecount < argcount:
+			self.msg = (
+			f"Enforcing only {typecount} {'type' if typecount == 1 else 'types'} "
+			f"while {argcount} {'argument exists' if argcount == 1 else 'arguments exist'}. "
+			f"Defined method, {funcname}(), may produce unexpected results."
+			)
+
+	def __str__(self):
+		return self.msg
