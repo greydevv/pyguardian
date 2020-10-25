0c72c1177fc2de4599f35299d72b11d94ce50ffe
Greyson Murray <greyson.murray@gmail.com>
2020-10-24 20:29:17 -0400

    add docstring for InvalidArgumentError

--- pyguard/errors/__init__.py
+++ pyguard/errors/__init__.py
@@ -1,8 +1,9 @@
 class ArgumentIncongruityWarning(Warning):
 	"""
-	Warning raised when number of type arguments do not match 
-	the function's number of parameters. This could mean both 
-	overconstraining and underconstraining the method's parameters.
+	ArgumentIncongruityWarning (subclass of Warning) is raised when 
+	number of type arguments do not match the function's number of 
+	parameters. This could mean both enforcing more types than there 
+	are parameters, or vice versa.
 	"""
 	def __init__(self, funcname, typecount, argcount):
 		if typecount > argcount:
@@ -21,6 +22,10 @@ class ArgumentIncongruityWarning(Warning):
 		return self.msg
 
 class InvalidArgumentError(TypeError):
+	"""
+	InvalidArgumentError (subclass of TypeError) is raised when the type of a 
+	value passed to a guarded method does not match the enforced type.
+	"""
 	def __init__(self, param, enforced_type, given_type):
 		self.error = (
 			f'Expected parameter "{param}" to be of type "{enforced_type}" but found "{given_type}"'
@@ -28,8 +33,3 @@ class InvalidArgumentError(TypeError):
 
 	def __str__(self):
 		return self.error
-
-
-"""
-str was enforced on parameter 'b' but found str
-"""
