1ac6e6ea4556e548358f46d3684e1345fcf1d0f6
Greyson Murray <greyson.murray@gmail.com>
2020-11-13 15:41:05 -0500

    explicitly checking for bool - bool is subclass of int: False == 0, True == 1

--- pyguard/core/findillegals.py
+++ pyguard/core/findillegals.py
@@ -1,7 +1,15 @@
 def findillegals(collection, valid_type):
 	if isinstance(collection, dict):
-		collection = collection.values()		
+			collection = collection.values()
+
 	for item in collection:
-		if not isinstance(item, valid_type):
+		# because 'bool' is a subclass of 'int,' it won't fail if 'True' or 'False' is passed while 'int' is enforced
+		if isinstance(item, bool):
+			if isinstance(valid_type, tuple):
+				if bool not in valid_type:
+					return type(item)
+			elif valid_type != bool:
+				return type(item)
+		elif not isinstance(item, valid_type):
 			return type(item)
 	return None
