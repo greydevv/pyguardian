fef22806cfb7dd0a298ba0e369f04a2db558e935
Greyson Murray <greyson.murray@gmail.com>
2020-12-21 19:27:42 -0500

    conclude v 0.0.1 testing

---
+++ tests/test_guard_class.py
@@ -0,0 +1,35 @@
+from pyguard import guard
+import pytest
+
+class TestClass:
+	@guard(object, int, bool)
+	def __init__(self, a, b):
+		pass
+
+	@guard(object, int)
+	def tc_instancemethod(self, c):
+		pass
+
+	@staticmethod
+	@guard(int)
+	def tc_staticmethod(d):
+		pass
+
+	@classmethod
+	@guard(object, int)
+	def tc_classmethod(cls, e):
+		pass
+
+def test_testclass_initmethod():
+	# this function must be manually tested because pytest cannot collect classes with __init__ methods
+	tc = TestClass(1, True)
+
+def test_testclass_instancemethod():
+	tc = TestClass(1, True)
+	tc.tc_instancemethod(1)
+
+def test_testclass_staticmethod():
+	TestClass.tc_staticmethod(1)
+
+def test_testclass_classmethod():
+	TestClass.tc_classmethod(1)
---
+++ tests/test_guard_successful.py
@@ -0,0 +1,57 @@
+from pyguard import guard
+
+@guard(bool, int)
+def foo(a, b):
+	pass
+
+@guard(bool, (int, float))
+def foo_multi(a, b):
+	pass
+
+@guard(bool, int)
+def bar(a, *args):
+	pass
+
+@guard(bool, (int, float))
+def bar_multi(a, *args):
+	pass
+
+@guard(int, str)
+def baz(a, **kwargs):
+	pass
+
+@guard(bool, (int, float))
+def baz_multi(a, **kwargs):
+	pass
+
+@guard(int, str)
+def qux(*args, **kwargs):
+	pass
+
+@guard((int, float), (bool, str))
+def qux_multi(*args, **kwargs):
+	pass
+
+def test_foo():
+	assert foo(True, 1) == None
+
+def test_foo_multi():
+	assert foo_multi(True, 1.2) == None
+
+def test_bar():
+	assert bar(True, 1, 2, 3, 4, 5) == None
+
+def test_bar_multi():
+	assert bar_multi(True, 1, 2, 3.4, 4, 5.6) == None
+
+def test_baz():
+	assert baz(1, x="Hello", y="World") == None
+
+def test_baz_multi():
+	assert baz_multi(True, x=1, y=2.3, z=3) == None
+
+def test_qux():
+	assert qux(1, 2, 3, x="Hello", y="World") == None
+
+def test_qux_multi():
+	assert qux_multi(1, 2.3, 4, 5.6, x=True, y="Hello") == None
