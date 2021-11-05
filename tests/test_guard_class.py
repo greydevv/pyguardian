88222bc4204ab96f2571d23664e6f0fafb51bf34
Greyson Murray <greyson.murray@gmail.com>
2021-11-05 16:46:52 -0400

    fix collection warning

--- tests/test_guard_class.py
+++ tests/test_guard_class.py
@@ -2,34 +2,35 @@ from pyguardian import guard
 import pytest
 
 class TestClass:
-	@guard(object, int, bool)
-	def __init__(self, a, b):
-		pass
+    __test__ = False
+    
+    @guard(object, int, bool)
+    def __init__(self, a, b):
+        pass
 
-	@guard(object, int)
-	def tc_instancemethod(self, c):
-		pass
+    @guard(object, int)
+    def tc_instancemethod(self, c):
+        pass
 
-	@staticmethod
-	@guard(int)
-	def tc_staticmethod(d):
-		pass
+    @staticmethod
+    @guard(int)
+    def tc_staticmethod(d):
+        pass
 
-	@classmethod
-	@guard(object, int)
-	def tc_classmethod(cls, e):
-		pass
+    @classmethod
+    @guard(object, int)
+    def tc_classmethod(cls, e):
+        pass
 
 def test_testclass_initmethod():
-	# this function must be manually tested because pytest cannot collect classes with __init__ methods
-	tc = TestClass(1, True)
+    tc = TestClass(1, True)
 
 def test_testclass_instancemethod():
-	tc = TestClass(1, True)
-	tc.tc_instancemethod(1)
+    tc = TestClass(1, True)
+    tc.tc_instancemethod(1)
 
 def test_testclass_staticmethod():
-	TestClass.tc_staticmethod(1)
+    TestClass.tc_staticmethod(1)
 
 def test_testclass_classmethod():
-	TestClass.tc_classmethod(1)
+    TestClass.tc_classmethod(1)
